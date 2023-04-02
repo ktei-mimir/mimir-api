cb04d2ea33f7ec5c532c6a7d5e4ce65630cd6524
ktei <ktei2008@gmail.com>
2023-04-03 01:43:03 +1000

    initial commit

---
+++ src/Mimir.Api/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Text.Json;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// logging
+builder.Logging
+    .ClearProviders()
+    .AddJsonConsole();
+
+// health checks
+builder.Services.AddHealthChecks();
+
+builder.Services
+    .AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+    });
+
+// add Swagger/OpenAPI support
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// add AWS Lambda support.
+builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
+
+// kestrel configs
+builder.WebHost.UseUrls("http://*:5000");
+
+var app = builder.Build();
+app.MapHealthChecks("/healthy");
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
+
+app.MapGet("/document/{name}", (string name) =>
+{
+    
+    return name;
+});
+
+await app.RunAsync();
