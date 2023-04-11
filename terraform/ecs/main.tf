5dea647d270d5eab6567a43f516c8872a812d47c
ktei <ktei2008@gmail.com>
2023-04-11 23:47:34 +1000

    WIP: deployment

--- terraform/ecs/main.tf
+++ terraform/ecs/main.tf
@@ -24,7 +24,9 @@ resource "aws_ecs_service" "service" {
   name    = "${var.app_name}-${var.environment}"
   cluster = local.cluster_name
   # task_definition = aws_ecs_task_definition.task.arn
-  desired_count = 1
+  desired_count                      = 1
+  deployment_maximum_percent         = 100
+  deployment_minimum_healthy_percent = 0
 
   ordered_placement_strategy {
     type  = "binpack"
