55a72d3482a31d3aa695729a78b9784cc20202c5
Rui Cheng <rui.cheng@sympil.com.au>
2023-04-11 11:49:16 +1000

    add terraform

---
+++ terraform/vpc/main.tf
@@ -0,0 +1,25 @@
+locals {
+  name = "${var.environment}-${var.vpc_name}"
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = local.name
+  cidr = "10.0.0.0/16"
+
+  azs            = ["ap-southeast-2a", "ap-southeast-2b"]
+  public_subnets = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway     = false
+  single_nat_gateway     = false
+  one_nat_gateway_per_az = false
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Terraform   = "true"
+    Environment = var.environment
+  }
+}
